ba96d228fb385f8152b054bf3b8d04437929b396
kushwanth <kushwanthvaddi3@gmail.com>
2024-08-05 17:44:20 +0530

    Fetching implemented in Schedule Management

--- src/main/java/com/ridetogether/repository/ScheduleRepository.java
+++ src/main/java/com/ridetogether/repository/ScheduleRepository.java
@@ -3,13 +3,10 @@ package com.ridetogether.repository;
 import com.ridetogether.model.Schedule;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends MongoRepository<Schedule, String> {
-    List<Schedule> findByScheduleDate(LocalDate date);
     List<Schedule> findByScheduleDateBetween(LocalDateTime start, LocalDateTime end);
 }
